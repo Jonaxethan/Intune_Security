645da84f457380bcc4e505fde066bce6e5989e02
Jonathan Axt <jonathanaxt@MacBook-Pro-von-Jonathan.local>
2025-08-07 08:43:35 +0200

    Errorhandling

--- EDR/Check-IntuneDefenderstatus.ps1
+++ EDR/Check-IntuneDefenderstatus.ps1
@@ -4,23 +4,26 @@
 #Description: This script checks the Status from MS Defender on Windows Devices
 
 $DefenderStatus = Get-MpComputerStatus
+$EXITCODE = 0
 Switch($DefenderStatus.AMRunningMode){
     "Active" {
         Write-Host "Microsoft Defender Antivirus is running in Active Mode." -ForegroundColor Green
     }
     "EDR Block Mode" {
-        Write-Host "Microsoft Defender Antivirus is running in Passive Mode." -ForegroundColor Yellow
+        Write-Host "Microsoft Defender Antivirus is running in Block Mode." -ForegroundColor Yellow
     }
     "passive" {
         Write-Host "Microsoft Defender Antivirus is running in Passive Mode." -ForegroundColor Yellow
-        exit 1
+        $EXITCODE = 1
     }
     "Disabled" {
         Write-Host "Microsoft Defender Antivirus is Disabled." -ForegroundColor Red
-        exit 1
+        $EXITCODE = 1
     }
     Default {
         Write-Host "Unknown status: $($DefenderStatus.AMRunningMode)" -ForegroundColor Red
-        exit 1
+        $EXITCODE = 1
     }
 }
+
+exit $EXITCODE
